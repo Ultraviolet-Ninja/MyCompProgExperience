47eb6faaf72ea28c27e8b920089da3459a20f172
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2022-11-04 20:03:35 -0500

    Got reseto

---
+++ src/kattis/club/week9/reseto.java
@@ -0,0 +1,60 @@
+package kattis.club.week9;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+public class reseto {
+    private static final StringBuilder BUFFER = new StringBuilder();
+    private static final BufferedReader IN = new BufferedReader(new InputStreamReader(System.in));
+    private static final OutputStream OUT = new BufferedOutputStream(System.out);
+
+    public static void main(String[] args) throws IOException {
+        String[] line = IN.readLine().split(" ");
+
+        int n = Integer.parseInt(line[0]);
+        int k = Integer.parseInt(line[1]);
+
+        Set<Integer> crossOuts = new HashSet<>(n);
+
+        int[] primes = IntStream.rangeClosed(2, n).filter(reseto::isPrime).toArray();
+
+        for (int prime : primes) {
+            for (int i = prime; i <= n; i += prime) {
+                if (!crossOuts.contains(i)) {
+                    crossOuts.add(i);
+                    k--;
+                }
+                if (k == 0) {
+                    OUT.write(BUFFER.append(i).append("\n").toString().getBytes());
+                    OUT.flush();
+                    return;
+                }
+            }
+        }
+    }
+
+    private static boolean isPrime(int n) {
+        if (n <= 3) {
+            return n > 1;
+        }
+
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+
+        int stop = (int) Math.sqrt(n);
+
+        for (int i = 5; i < stop; i+= 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/kattis/club/week9/reseto1.java
@@ -0,0 +1,52 @@
+package kattis.club.week9;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+public class reseto1 {
+    private static final StringBuilder BUFFER = new StringBuilder();
+    private static final BufferedReader IN = new BufferedReader(new InputStreamReader(System.in));
+    private static final OutputStream OUT = new BufferedOutputStream(System.out);
+
+    public static void main(String[] args) throws IOException {
+        String[] line = IN.readLine().split(" ");
+        Set<Integer> crossOuts = new HashSet<>();
+
+        int n = Integer.parseInt(line[0]);
+        int k = Integer.parseInt(line[1]);
+        List<Integer> list = new ArrayList<>();
+
+        IntStream.rangeClosed(2, n).forEach(list::add);
+
+        while (k != 0) {
+            int p = -1;
+            for (int num : list) {
+                if (!crossOuts.contains(num) && p == -1) {
+                    p = num;
+                }
+
+                if (!crossOuts.contains(num) && num % p == 0) {
+                    crossOuts.add(num);
+                    k--;
+                }
+
+                if (k == 0) {
+                    BUFFER.append(num).append("\n");
+                    break;
+                }
+            }
+            list.removeIf(crossOuts::contains);
+        }
+
+        OUT.write(BUFFER.toString().getBytes());
+        OUT.flush();
+    }
+}
