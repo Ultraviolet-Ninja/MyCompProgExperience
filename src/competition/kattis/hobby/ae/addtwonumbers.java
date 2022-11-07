64f4f5977221c612d8614969b7a3d672143c90ea
Mr. J <45538844+Ultraviolet-Ninja@users.noreply.github.com>
2022-11-07 10:42:58 -0600

    lol

---
+++ src/competition/kattis/hobby/ae/addtwonumbers.java
@@ -0,0 +1,22 @@
+package competition.kattis.hobby.ae;
+
+import competition.annotations.Done;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Done(url = "https://open.kattis.com/problems/addtwonumbers")
+public class addtwonumbers {
+    private static final StringBuilder BUFFER = new StringBuilder();
+    private static final BufferedReader IN = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedOutputStream OUT = new BufferedOutputStream(System.out);
+
+    public static void main(String[] args) throws IOException {
+        String[] line = IN.readLine().split(" ");
+        BUFFER.append(Integer.parseInt(line[0]) + Integer.parseInt(line[1]));
+        OUT.write(BUFFER.toString().getBytes());
+        OUT.flush();
+    }
+}
